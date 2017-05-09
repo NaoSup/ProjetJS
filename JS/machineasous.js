75c9925b2f7bf829c1ad420daf80eb1426d2546c
Naomi <npaulmin@yahoo.Com>
2017-05-09 15:50:29 +0200

    mise en place du score + arret du jeu à 0 crédits

--- JS/machineasous.js
+++ JS/machineasous.js
@@ -1,32 +1,42 @@
-var credits = $('#credits').val();
-    
-$('button').click(
-    function random (e) {
-        //On retire 1 crédit
+$('#boutonJouer').prop('disabled', false);
+
+$('#boutonJouer').click(
+    function random() {
+        var credits = $('#credits').val();
+        var score = $('#score').val();
+        
+        //On retire 1 crédit par tour joué
         credits -= 1;
         $('#credits').attr('value', credits);
-        
+        console.log("ok");
+
         //On génère l'indice des images aléatoires
         var a = Math.floor(Math.random() * 4);
         var b = Math.floor(Math.random() * 4);
         var c = Math.floor(Math.random() * 4);
         var d = Math.floor(Math.random() * 4);
-        
+
         //On modifie les sources des images en fonction de l'indice précédent
         $('#img1').attr('src', '../img/img' + a + '.png');
         $('#img2').attr('src', '../img/img' + b + '.png');
         $('#img3').attr('src', '../img/img' + c + '.png');
         $('#img4').attr('src', '../img/img' + d + '.png');
-        
+
         //si les 4 symboles sont identiques on rajoute 5 crédits
         if (a == b && b == c & c == d) {
             credits += 5;
             $('#credits').attr('value', credits);
         }
         
+         //On récupère le meilleur score
+        if (credits > score) {
+            score = credits;
+            $("#score").attr('value', score);
+        }
+        
         if (credits == 0) {
-            e.preventDefault();
-            $('#perdu').text("Vous n'avez plus de crédits");
+            $('#boutonJouer').prop('disabled', true);
+            $('#message').text("Dommage ! Vous n'avez plus de crédits...");
         }
     }
 );
