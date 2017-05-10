1a69c6fe9842614e8b1a602e3e30f25269fdc2cb
Naomi <npaulmin@yahoo.Com>
2017-05-10 14:08:51 +0200

    ajout footer + animation images

--- JS/machineasous.js
+++ JS/machineasous.js
@@ -17,10 +17,10 @@ $('#boutonJouer').click(
         var d = Math.floor(Math.random() * 4);
 
         //On modifie les sources des images en fonction de l'indice précédent
-        $('#img1').attr('src', '../img/img' + a + '.png');
-        $('#img2').attr('src', '../img/img' + b + '.png');
-        $('#img3').attr('src', '../img/img' + c + '.png');
-        $('#img4').attr('src', '../img/img' + d + '.png');
+        $('#img1').attr('src', '../img/img' + a + '.png').fadeToggle(0).fadeToggle(200);
+        $('#img2').attr('src', '../img/img' + b + '.png').fadeToggle(0).fadeToggle(200);
+        $('#img3').attr('src', '../img/img' + c + '.png').fadeToggle(0).fadeToggle(200);
+        $('#img4').attr('src', '../img/img' + d + '.png').fadeToggle(0).fadeToggle(200);
 
         //si les 4 symboles sont identiques on rajoute 5 crédits
         if (a == b && b == c & c == d) {
@@ -36,7 +36,7 @@ $('#boutonJouer').click(
         
         if (credits == 0) {
             $('#boutonJouer').prop('disabled', true);
-            $('#message').text("Dommage ! Vous n'avez plus de crédits...");
+            $('#message').text("Dommage ! C'est perdu. Vous n'avez plus de crédits...");
         }
     }
 );
